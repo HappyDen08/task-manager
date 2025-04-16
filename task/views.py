e5979d885afc92347214a29524751fe15240f4f3
Denys Dikhtiar <dinis.dihtjar08@gmail.com>
2025-04-16 15:47:07 +0300

    add task detail view

--- task/views.py
+++ task/views.py
@@ -14,3 +14,9 @@ class Home(generic.ListView):
     context_object_name = "task_list"
     template_name = "task/task_list.html"
     paginate_by = 5
+
+
+class TaskDetail(generic.DetailView):
+    model = Task
+    context_object_name = "task_detail"
+    template_name = "task/task_detail.html"
