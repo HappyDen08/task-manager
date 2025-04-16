4ebc3ea07eafcb83349e1bc377d564de6412e544
Denys Dikhtiar <dinis.dihtjar08@gmail.com>
2025-04-16 14:13:58 +0300

    add simple view for home page

--- task/views.py
+++ task/views.py
@@ -1,6 +1,16 @@
+from .models import (
+    Task,
+    TaskType,
+    Worker,
+    Position
+)
+
 from django.shortcuts import render
 from django.views import generic
 
 
 class Home(generic.ListView):
-    pass
+    model = Task
+    context_object_name = "task_list"
+    template_name = "task/task_list.html"
+    paginate_by = 5
