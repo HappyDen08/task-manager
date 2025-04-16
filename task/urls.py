60e7ffbead752e38989e55a7a573ac288e35b19b
Denys Dikhtiar <dinis.dihtjar08@gmail.com>
2025-04-16 22:58:08 +0300

    add new route and change any old

--- task/urls.py
+++ task/urls.py
@@ -2,16 +2,21 @@ from django.urls import path
 
 from .views import (
     HomeView,
+    TaskListView,
     TaskDetailView,
     TaskCreateView,
     TaskUpdateView,
     TaskDeleteView,
-    WorkersListView, WorkersDetailView, WorkersUpdateView, WorkersDeleteView
+    WorkersListView,
+    WorkersDetailView,
+    WorkersUpdateView,
+    WorkersDeleteView
 )
 
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
+    path("task/", TaskListView.as_view(), name="task_list"),
     path("task/<int:pk>/", TaskDetailView.as_view(), name="task_detail"),
     path("task/create/", TaskCreateView.as_view(), name="task_create"),
     path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task_update"),
