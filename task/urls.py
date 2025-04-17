a86bed78243a2a0e5e8af453e322fa3c9b6acfc5
Denys Dikhtiar <dinis.dihtjar08@gmail.com>
2025-04-17 12:30:41 +0300

    add new route for toggle done

--- task/urls.py
+++ task/urls.py
@@ -8,7 +8,10 @@ from .views import (
     TaskUpdateView,
     TaskDeleteView,
     TaskCreateMyselfView,
+    TaskMyselfDetailView,
+    TaskMyselfUpdateView,
     WorkersListView,
+    toggle_done,
 )
 
 
@@ -16,11 +19,14 @@ urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("task/", TaskListView.as_view(), name="task_list"),
     path("task/<int:pk>/", TaskDetailView.as_view(), name="task_detail"),
+    path("task/myself/<int:pk>/", TaskMyselfDetailView.as_view(), name="task_myself_detail"),
     path("task/create/", TaskCreateView.as_view(), name="task_create"),
     path("task/myself/create", TaskCreateMyselfView.as_view(), name="task_myself_create"),
     path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task_update"),
+    path("task/<int:pk>/myself/update/", TaskMyselfUpdateView.as_view(), name="task_myself_update"),
     path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task_delete"),
     path("workers/", WorkersListView.as_view(), name="workers_list"),
+    path("task/<int:pk>/toggle_done/", toggle_done, name="toggle_done"),
 ]
 
 app_name = "task"
