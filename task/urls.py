68643113fce37477028de31cb71c0540d16de59b
Denys Dikhtiar <dinis.dihtjar08@gmail.com>
2025-04-17 10:54:11 +0300

    remove useless link

--- task/urls.py
+++ task/urls.py
@@ -7,10 +7,8 @@ from .views import (
     TaskCreateView,
     TaskUpdateView,
     TaskDeleteView,
+    TaskCreateMyselfView,
     WorkersListView,
-    WorkersDetailView,
-    WorkersUpdateView,
-    WorkersDeleteView
 )
 
 
@@ -19,12 +17,10 @@ urlpatterns = [
     path("task/", TaskListView.as_view(), name="task_list"),
     path("task/<int:pk>/", TaskDetailView.as_view(), name="task_detail"),
     path("task/create/", TaskCreateView.as_view(), name="task_create"),
+    path("task/myself/create", TaskCreateMyselfView.as_view(), name="task_myself_create"),
     path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task_update"),
     path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task_delete"),
     path("workers/", WorkersListView.as_view(), name="workers_list"),
-    path("workers/<int:pk>/", WorkersDetailView.as_view(), name="workers_detail"),
-    path("workers/<int:pk>/update/", WorkersUpdateView.as_view(), name="workers_update"),
-    path("workers/<int:pk>/delete/", WorkersDeleteView.as_view(), name="workers_delete")
 ]
 
 app_name = "task"
