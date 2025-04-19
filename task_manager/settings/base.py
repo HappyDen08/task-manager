f4c2de84f4c0c332bc52ecafa35ff45e9b61ed5b
Denys Dikhtiar <dinis.dihtjar08@gmail.com>
2025-04-19 22:15:03 +0300

    add all project with deploy version

--- task_manager/settings/base.py
+++ task_manager/settings/base.py
@@ -15,7 +15,7 @@ from pathlib import Path
 from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -24,10 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-iy!n0)_+vqgdsvyqev0ontkr8c=vrc7zc50mbn7nrb8-o5lg9q")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -73,15 +70,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "task_manager.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
 
 # Password validation
